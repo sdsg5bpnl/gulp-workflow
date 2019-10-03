8e4dca258b72410fc8ba345e15ebabd6c8044528
Hercules Lin <sdsg5bpnl@outlook.com>
2019-10-03 13:21:29 +0800

    modify src folder, add css and imagemin support

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,129 @@
 const { series, src, dest, watch } = require('gulp');
+const parseArgs = require('minimist');
 const browserSync = require('browser-sync').create();
+const gulpif = require('gulp-if');
 const htmlmin = require('gulp-htmlmin');
 const pug = require('gulp-pug');
 const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
 const dartSass = require('gulp-dart-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
-function HTML() {
-  return src('src/**/*.html')
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
+const envOptions = {
+  string: 'env',
+  default: { env: 'develop' },
+};
+const options = parseArgs(process.argv.slice(2), envOptions);
 
-function minifyHTML() {
-  return src('src/**/*.html')
+function copyHTML() {
+  return src('src/*.html')
     .pipe(
-      htmlmin({
-        collapseWhitespace: true,
-        removeComments: true,
-      }),
+      gulpif(
+        options.env === 'production',
+        htmlmin({ collapseWhitespace: true, removeComments: true }),
+      ),
     )
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
 function pugToHTML() {
-  return src('src/**/*.pug')
-    .pipe(
-      pug({
-        pretty: true,
-      }),
-    )
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
-
-function pugToMinifyHTML() {
-  return src('src/**/*.pug')
-    .pipe(pug({}))
+  return src('src/*.pug')
+    .pipe(gulpif(options.env === 'production', pug({}), pug({ pretty: true })))
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
 function ejsToHTML() {
-  return src('src/**/*.ejs')
-    .pipe(ejs({}))
-    .pipe(rename({ extname: '.html' }))
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
-
-function ejsToMinifyHTML() {
-  return src('src/**/*.ejs')
+  return src('src/*.ejs')
     .pipe(ejs({}))
     .pipe(rename({ extname: '.html' }))
     .pipe(
-      htmlmin({
-        collapseWhitespace: true,
-        removeComments: true,
-      }),
+      gulpif(
+        options.env === 'production',
+        htmlmin({ collapseWhitespace: true, removeComments: true }),
+      ),
     )
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
-function sass() {
-  return src('src/**/*.scss')
+function copyCSS() {
+  return src('src/styles/**/*.css')
+    .pipe(gulpif(options.env === 'production', sourcemaps.init()))
+    .pipe(
+      gulpif(
+        options.env === 'production',
+        cleanCSS({ compatibility: 'ie8', debug: true }, details => {
+          console.log(`${details.name}: ${details.stats.originalSize}`);
+          console.log(`${details.name}: ${details.stats.minifiedSize}`);
+        }),
+      ),
+    )
+    .pipe(gulpif(options.env === 'production', sourcemaps.write('.')))
+    .pipe(dest('dist/styles'))
+    .pipe(browserSync.stream());
+}
+
+function scssToCSS() {
+  return src('src/styles/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(
-      dartSass({
-        includePaths: ['./node_modules/bootstrap/scss'],
-      }).on('error', dartSass.logError),
+      gulpif(
+        options.env === 'production',
+        dartSass({
+          outputStyle: 'compressed',
+          includePaths: ['./node_modules/bootstrap/scss'],
+        }).on('error', dartSass.logError),
+        dartSass({
+          includePaths: ['./node_modules/bootstrap/scss'],
+        }).on('error', dartSass.logError),
+      ),
     )
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist'))
+    .pipe(dest('dist/styles'))
     .pipe(browserSync.stream());
 }
 
-function minifySass() {
-  return src('src/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(
-      dartSass({
-        outputStyle: 'compressed',
-        includePaths: ['./node_modules/bootstrap/scss'],
-      }).on('error', dartSass.logError),
-    )
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
-
-function javascript() {
-  return src('src/**/*.js')
-    .pipe(
-      babel({
-        presets: ['@babel/env'],
-      }),
-    )
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
-
-function minifyJS() {
-  return src('src/**/*.js')
+function copyJS() {
+  return src('src/scripts/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(
       babel({
         presets: ['@babel/env'],
       }),
     )
-    .pipe(uglify())
+    .pipe(concat('vendor.js'))
+    .pipe(gulpif(options.env === 'production', uglify()))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist'))
+    .pipe(dest('dist/scripts'))
     .pipe(browserSync.stream());
 }
 
-function typescript() {
-  return src('src/**/*.ts')
+function tsToJS() {
+  return src('src/scripts/**/*.ts')
     .pipe(sourcemaps.init())
     .pipe(tsProject())
+    .pipe(gulpif(options.env === 'production', uglify()))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist'))
+    .pipe(dest('dist/scripts'))
     .pipe(browserSync.stream());
 }
 
-function minifyTS() {
-  return src('src/**/*.ts')
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist'))
+function copyImages() {
+  return src('src/images/**/*')
+    .pipe(gulpif(options.env === 'production', imagemin()))
+    .pipe(dest('dist/images'))
     .pipe(browserSync.stream());
 }
 
@@ -156,32 +139,38 @@ function serve() {
       baseDir: './dist',
     },
   });
-  watch('src/**/*.html', series(HTML));
+  watch('src/*.html', series(copyHTML));
   watch('src/**/*.pug', series(pugToHTML));
   watch('src/**/*.ejs', series(ejsToHTML));
-  watch('src/**/*.scss', series(sass));
-  watch('src/**/*.js', series(javascript));
-  watch('src/**/*.ts', series(typescript));
+  watch('src/styles/**/*.css', series(copyCSS));
+  watch('src/styles/**/*.scss', series(scssToCSS));
+  watch('src/scripts/**/*.js', series(copyJS));
+  watch('src/scripts/**/*.ts', series(tsToJS));
+  watch('src/images/**/*', series(copyImages));
   watch('src/static/**/*', series(copyStatic));
 }
 
 exports.default = series(
-  HTML,
+  copyHTML,
   pugToHTML,
   ejsToHTML,
-  sass,
-  javascript,
-  typescript,
+  copyCSS,
+  scssToCSS,
+  copyJS,
+  tsToJS,
+  copyImages,
   copyStatic,
   serve,
 );
 
 exports.build = series(
-  minifyHTML,
-  pugToMinifyHTML,
-  ejsToMinifyHTML,
-  minifySass,
-  minifyJS,
-  minifyTS,
+  copyHTML,
+  pugToHTML,
+  ejsToHTML,
+  copyCSS,
+  scssToCSS,
+  copyJS,
+  tsToJS,
+  copyImages,
   copyStatic,
 );
